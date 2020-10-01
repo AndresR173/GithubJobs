6522425524695f14a7fea46fa4b0252fb0b6ffb2
Andres Rojas <andres.rojas@endava.com>
2020-10-01 10:10:02 -0500

    Initial Project

---
+++ GithubJobs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubJobs
+//
+//  Created by Andres Felipe Rojas R. on 1/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubJobs/GithubJobsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubJobsApp.swift
+//  GithubJobs
+//
+//  Created by Andres Felipe Rojas R. on 1/10/20.
+//
+
+import SwiftUI
+
+@main
+struct GithubJobsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
